af5ef4adfb39721a56ac6f1a3d428f2a672e0858
Smart781 <Artems2311@mail.ru>
2024-09-20 11:27:45 +0300

    logs

--- log-files/copy.go
+++ log-files/copy.go
@@ -1,98 +1,63 @@
-/*
-Copyright (c) 2009 The Go Authors. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-   * Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-   * Redistributions in binary form must reproduce the above
-copyright notice, this list of conditions and the following disclaimer
-in the documentation and/or other materials provided with the
-distribution.
-   * Neither the name of Google Inc. nor the names of its
-contributors may be used to endorse or promote products derived from
-this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-/*
-Forked from https://golang.org/src/io/io.go
-*/
 package common
 
 import (
 	"io"
 	"net"
-        "log"
+	//"logging"
 )
 
 func Copy(dst net.Conn, src net.Conn) (written int64, err error) {
 	defer func() {
-		log.Printf("Cloak/Copy: Closing source and destination connections")
+		logging.Info.Printf("Cloak/Copy: Closing source and destination connections")
 		src.Close()
 		dst.Close()
 	}()
 
-	// If the reader has a WriteTo method, use it to do the copy.
-	// Avoids an allocation and a copy.
 	if wt, ok := src.(io.WriterTo); ok {
-		log.Printf("Cloak/Copy: src implements WriteTo, using WriteTo for copy")
+		logging.Info.Printf("Cloak/Copy: src implements WriteTo, using WriteTo for copy")
 		return wt.WriteTo(dst)
 	}
 
-	// Similarly, if the writer has a ReadFrom method, use it to do the copy.
 	if rt, ok := dst.(io.ReaderFrom); ok {
-		log.Printf("Cloak/Copy: dst implements ReadFrom, using ReadFrom for copy")
+		logging.Info.Printf("Cloak/Copy: dst implements ReadFrom, using ReadFrom for copy")
 		return rt.ReadFrom(src)
 	}
 
 	size := 32 * 1024
 	buf := make([]byte, size)
-	log.Printf("Cloak/Copy: Buffer of size %d bytes created", size)
+	logging.Info.Printf("Cloak/Copy: Buffer of size %d bytes created", size)
 
 	for {
-		log.Printf("Cloak/Copy: Attempting to read from src")
+		logging.Info.Printf("Cloak/Copy: Attempting to read from src")
 		nr, er := src.Read(buf)
 		if nr > 0 {
-			log.Printf("Cloak/Copy: Read %d bytes from src", nr)
+			logging.Info.Printf("Cloak/Copy: Read %d bytes from src", nr)
 			nw, ew := dst.Write(buf[0:nr])
 			if nw > 0 {
 				written += int64(nw)
-				log.Printf("Cloak/Copy: Wrote %d bytes to dst, total written: %d", nw, written)
+				logging.Info.Printf("Cloak/Copy: Wrote %d bytes to dst, total written: %d", nw, written)
 			}
 			if ew != nil {
 				err = ew
-				log.Printf("Cloak/Copy: Error writing to dst: %v", ew)
+				logging.Info.Printf("Cloak/Copy: Error writing to dst: %v", ew)
 				break
 			}
 			if nr != nw {
 				err = io.ErrShortWrite
-				log.Printf("Cloak/Copy: Short write, expected %d bytes, but wrote %d bytes", nr, nw)
+				logging.Info.Printf("Cloak/Copy: Short write, expected %d bytes, but wrote %d bytes", nr, nw)
 				break
 			}
 		}
 		if er != nil {
 			if er != io.EOF {
 				err = er
-				log.Printf("Cloak/Copy: Error reading from src: %v", er)
+				logging.Info.Printf("Cloak/Copy: Error reading from src: %v", er)
 			} else {
-				log.Printf("Cloak/Copy: End of file (EOF) reached")
+				logging.Info.Printf("Cloak/Copy: End of file (EOF) reached")
 			}
 			break
 		}
 	}
-	log.Printf("Cloak/Copy: Copy operation completed, total bytes written: %d, error: %v", written, err)
+	logging.Info.Printf("Cloak/Copy: Copy operation completed, total bytes written: %d, error: %v", written, err)
 	return written, err
 }
--- log-files/piper.go
+++ log-files/piper.go
@@ -5,18 +5,30 @@ import (
 	"net"
 	"sync"
 	"time"
+        "os"
+        "log"
 
 	"github.com/cbeuw/Cloak/internal/common"
 
 	mux "github.com/cbeuw/Cloak/internal/multiplex"
-	log "github.com/sirupsen/logrus"
+	//"logging"
 )
+
+var logging = &struct {
+	Debug, Info, Warn, Err *log.Logger
+}{
+	Debug: log.New(io.Discard, "[DEBUG] ", log.LstdFlags),
+	Info:  log.New(os.Stdout, "[INFO] ", log.LstdFlags),
+	Warn:  log.New(os.Stderr, "[WARN] ", log.LstdFlags),
+	Err:   log.New(os.Stderr, "[ERROR] ", log.LstdFlags),
+}
+
 
 func RouteUDP(bindFunc func() (*net.UDPConn, error), streamTimeout time.Duration, singleplex bool, newSeshFunc func() *mux.Session) {
 	var sesh *mux.Session
 	localConn, err := bindFunc()
 	if err != nil {
-		log.Fatal(err)
+		logging.Fatal(err)
 	}
 
 	streams := make(map[string]*mux.Stream)
@@ -26,7 +38,7 @@ func RouteUDP(bindFunc func() (*net.UDPConn, error), streamTimeout time.Duration
 	for {
 		i, addr, err := localConn.ReadFrom(data)
 		if err != nil {
-			log.Errorf("Failed to read first packet from proxy client: %v", err)
+			logging.Info.Printf("Failed to read first packet from proxy client: %v", err)
 			continue
 		}
 
@@ -46,7 +58,7 @@ func RouteUDP(bindFunc func() (*net.UDPConn, error), streamTimeout time.Duration
 				if singleplex {
 					sesh.Close()
 				}
-				log.Errorf("Failed to open stream: %v", err)
+				logging.Info.Printf("Failed to open stream: %v", err)
 				streamsMutex.Unlock()
 				continue
 			}
@@ -61,14 +73,14 @@ func RouteUDP(bindFunc func() (*net.UDPConn, error), streamTimeout time.Duration
 				for {
 					n, err := stream.Read(buf)
 					if err != nil {
-						log.Tracef("copying stream to proxy client: %v", err)
+						logging.Trace.Printf("copying stream to proxy client: %v", err)
 						break
 					}
 					_ = stream.SetReadDeadline(time.Now().Add(streamTimeout))
 
 					_, err = localConn.WriteTo(buf[:n], proxyAddr)
 					if err != nil {
-						log.Tracef("copying stream to proxy client: %v", err)
+						logging.Trace.Printf("copying stream to proxy client: %v", err)
 						break
 					}
 				}
@@ -84,7 +96,7 @@ func RouteUDP(bindFunc func() (*net.UDPConn, error), streamTimeout time.Duration
 
 		_, err = stream.Write(data[:i])
 		if err != nil {
-			log.Tracef("copying proxy client to stream: %v", err)
+			logging.Trace.Printf("copying proxy client to stream: %v", err)
 			streamsMutex.Lock()
 			delete(streams, addr.String())
 			streamsMutex.Unlock()
@@ -97,22 +109,20 @@ func RouteUDP(bindFunc func() (*net.UDPConn, error), streamTimeout time.Duration
 
 func RouteTCP(listener net.Listener, streamTimeout time.Duration, singleplex bool, newSeshFunc func() *mux.Session) {
 	var sesh *mux.Session
-	log.Printf("Cloak/RouteTCP: Starting TCP route. Stream timeout: %v, Singleplex: %v", streamTimeout, singleplex)
+	logging.Info.Printf("Cloak/RouteTCP: Starting TCP route. Stream timeout: %v, Singleplex: %v", streamTimeout, singleplex)
 
 	for {
 		// Принимаем новое соединение
 		localConn, err := listener.Accept()
 		if err != nil {
-			log.Printf("Cloak/RouteTCP: Failed to accept connection: %v", err)
+			logging.Info.Printf("Cloak/RouteTCP: Failed to accept connection: %v", err)
 			continue
 		}
-		log.Printf("Cloak/RouteTCP: Accepted new connection from %v", localConn.RemoteAddr())
+		logging.Info.Printf("Cloak/RouteTCP: Accepted new connection from %v", localConn.RemoteAddr())
 
 		// Создаем новую сессию, если нужно
 		if !singleplex && (sesh == nil || sesh.IsClosed()) {
-			log.Printf("Cloak/RouteTCP: Creating new session")
-			log.Printf("Cloak/RouteTCP: sesh = newSeshFunc()")
-			log.Printf("Cloak/RouteTCP: Enter the function MakeSession()")
+			logging.Info.Printf("Cloak/RouteTCP: Creating new session")
 			sesh = newSeshFunc()
 		}
 
@@ -120,77 +130,68 @@ func RouteTCP(listener net.Listener, streamTimeout time.Duration, singleplex boo
 		go func(sesh *mux.Session, localConn net.Conn, timeout time.Duration) {
 			// Если singleplex, создаем сессию для каждого соединения
 			if singleplex {
-				log.Printf("Cloak/RouteTCP: Singleplex mode: creating new session for this connection")
-				log.Printf("Cloak/RouteTCP: sesh = newSeshFunc()")
-				log.Printf("Cloak/RouteTCP: Enter the function MakeSession()")
+				logging.Info.Printf("Cloak/RouteTCP: Singleplex mode: creating new session for this connection")
 				sesh = newSeshFunc()
 			}
 
-			// Логируем строковую команду для чтения данных
 			data := make([]byte, 10240)
-			log.Printf("Cloak/RouteTCP: Setting read deadline for connection: %v", timeout)
+			logging.Info.Printf("Cloak/RouteTCP: Setting read deadline for connection: %v", timeout)
 			err := localConn.SetReadDeadline(time.Now().Add(streamTimeout))
 			if err != nil {
-				log.Printf("Cloak/RouteTCP: Failed to set read deadline: %v", err)
+				logging.Info.Printf("Cloak/RouteTCP: Failed to set read deadline: %v", err)
 				localConn.Close()
 				return
 			}
 
-			// Чтение данных от клиента
 			i, err := io.ReadAtLeast(localConn, data, 1)
 			if err != nil {
-				log.Printf("Cloak/RouteTCP: Failed to read first packet from proxy client: %v", err)
+				logging.Info.Printf("Cloak/RouteTCP: Failed to read first packet from proxy client: %v", err)
 				localConn.Close()
 				return
 			}
-			log.Printf("Cloak/RouteTCP: Read %d bytes from proxy client", i)
+			logging.Info.Printf("Cloak/RouteTCP: Read %d bytes from proxy client", i)
 
-			// Сброс времени ожидания чтения
 			var zeroTime time.Time
-			log.Printf("Cloak/RouteTCP: Resetting read deadline")
+			logging.Info.Printf("Cloak/RouteTCP: Resetting read deadline")
 			err = localConn.SetReadDeadline(zeroTime)
 			if err != nil {
-				log.Printf("Cloak/RouteTCP: Failed to reset read deadline: %v", err)
+				logging.Info.Printf("Cloak/RouteTCP: Failed to reset read deadline: %v", err)
 			}
 
-			// Открытие нового потока
 			stream, err := sesh.OpenStream()
 			if err != nil {
-				log.Printf("Cloak/RouteTCP: Failed to open stream: %v", err)
+				logging.Info.Printf("Cloak/RouteTCP: Failed to open stream: %v", err)
 				localConn.Close()
 				if singleplex {
 					sesh.Close()
 				}
 				return
 			}
-			log.Printf("Cloak/RouteTCP: Opened new stream for session %v", sesh)
+			logging.Info.Printf("Cloak/RouteTCP: Opened new stream for session %v", sesh)
 
-			// Отправка данных в поток
 			_, err = stream.Write(data[:i])
 			if err != nil {
-				log.Printf("Cloak/RouteTCP: Failed to write to stream: %v", err)
+				logging.Info.Printf("Cloak/RouteTCP: Failed to write to stream: %v", err)
 				localConn.Close()
 				stream.Close()
 				return
 			}
-			log.Printf("Cloak/RouteTCP: Successfully wrote %d bytes to stream", i)
+			logging.Info.Printf("Cloak/RouteTCP: Successfully wrote %d bytes to stream", i)
 
-			// Создаем горутину для копирования данных от клиента в поток
 			go func() {
-				log.Printf("Cloak/RouteTCP: Starting to copy data from proxy client to stream")
+				logging.Info.Printf("Cloak/RouteTCP: Starting to copy data from proxy client to stream")
 				if _, err := common.Copy(localConn, stream); err != nil {
-					log.Printf("Cloak/RouteTCP: Error copying data from proxy client to stream: %v", err)
+					logging.Info.Printf("Cloak/RouteTCP: Error copying data from proxy client to stream: %v", err)
 				} else {
-					log.Printf("Cloak/RouteTCP: Finished copying data from proxy client to stream")
+					logging.Info.Printf("Cloak/RouteTCP: Finished copying data from proxy client to stream")
 				}
 			}()
 
-			// Копируем данные от потока к клиенту
-			log.Printf("Cloak/RouteTCP: Starting to copy data from stream to proxy client")
+			logging.Info.Printf("Cloak/RouteTCP: Starting to copy data from stream to proxy client")
 			if _, err = common.Copy(stream, localConn); err != nil {
-				log.Printf("Cloak/RouteTCP: Error copying data from stream to proxy client: %v", err)
+				logging.Info.Printf("Cloak/RouteTCP: Error copying data from stream to proxy client: %v", err)
 			} else {
-				log.Printf("Cloak/RouteTCP: Finished copying data from stream to proxy client")
+				logging.Info.Printf("Cloak/RouteTCP: Finished copying data from stream to proxy client")
 			}
 		}(sesh, localConn, streamTimeout)
 	}
