3f87c6555756270f4833ea293f9bea6f0cae071a
Smart781 <Artems2311@mail.ru>
2024-09-22 15:18:14 +0300

    ck_client

---
+++ log-files/logging.go
@@ -0,0 +1,16 @@
+package out
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+var Logging = &struct {
+	Debug, Info, Warn, Err *log.Logger
+}{
+	Debug: log.New(io.Discard, "[DEBUG] ", log.LstdFlags),
+	Info:  log.New(os.Stdout, "[INFO] ", log.LstdFlags),
+	Warn:  log.New(os.Stderr, "[WARN] ", log.LstdFlags),
+	Err:   log.New(os.Stderr, "[ERROR] ", log.LstdFlags),
+}
--- log-files/piper.go
+++ log-files/piper.go
@@ -9,12 +9,13 @@ import (
         //"log"
 
 	"github.com/cbeuw/Cloak/internal/common"
+        "github.com/cbeuw/Cloak/internal/out"
 
 	mux "github.com/cbeuw/Cloak/internal/multiplex"
 	//"logging"
 )
 
-var logging = main.Logging
+var logging = out.Logging
 
 func RouteUDP(bindFunc func() (*net.UDPConn, error), streamTimeout time.Duration, singleplex bool, newSeshFunc func() *mux.Session) {
 	var sesh *mux.Session
